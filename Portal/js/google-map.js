3e2e11830cc61eac0e7cfc8f6aec1d3e7b3a5e1a
Jocellyn Luna <jmluna@espol.edu.ec>
2020-07-08 03:38:49 -0500

    se crea la opción Equipo, se empieza a modificar contacto, falta especificar a fondo los elementos del form. API de Google maps funcional. falta key

--- Portal/js/google-map.js
+++ Portal/js/google-map.js
@@ -1,62 +1,106 @@
 
-var google;
+var googleMap;
+var googleServicioDirecciones;
+//-2.242921, -79.894424
+var posicionInicial = { lat: -2.242921, lng: -79.894424};
+var marcadoresActuales = [];
+var rutasActuales = [];
+var mensajesError = "";
+var mensajesAdvertencia = "";
+var infoWindow;
 
 function init() {
-    // Basic options for a simple Google Map
-    // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-    // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
-    var myLatlng = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
-    // 39.399872
-    // -8.224454
-    
-    var mapOptions = {
-        // How zoomed in you want the map to start at (always required)
-        zoom: 7,
+    // Crear el mapa y centrarlo en la posición inicial
+    googleMap = new google.maps.Map(document.getElementById('map'), {
+        center: posicionInicial,
+        zoom: 16
+    });
 
-        // The latitude and longitude to center the map (always required)
-        center: myLatlng,
+    infoWindow = new google.maps.InfoWindow;
 
-        // How you would like to style the map. 
-        scrollwheel: false,
-        styles: [
-            {
-                "featureType": "administrative.country",
-                "elementType": "geometry",
-                "stylers": [
-                    {
-                        "visibility": "simplified"
-                    },
-                    {
-                        "hue": "#ff0000"
-                    }
-                ]
+    // Instanciar el servicio de direcciones
+    googleServicioDirecciones = new google.maps.DirectionsService;
+
+    //geolocalización del usuario
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+          var pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          };
+          createRoute(pos);
+        }, function() {
+          handleLocationError(true, infoWindow, googleMap.getCenter());
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, googleMap.getCenter());
+    }
+}
+
+function createRoute(pos){
+    console.log(pos);
+    console.log(posicionInicial);
+
+    if (pos !== null) {
+        // Solicitar las indicaciones para viajar entre las rutas indicadas
+        googleServicioDirecciones.route({
+            origin: { lat: pos.lat, lng: pos.lng},
+            destination: { lat: posicionInicial.lat, lng: posicionInicial.lng },
+            travelMode: 'DRIVING'
+        }, function(response, status) {
+            // Procesar la respuesta recibida...
+            if (status === 'OK') {
+                if (response.routes[0].warnings.length > 0) {
+                    mensajesAdvertencia += response.routes[0].warnings + "\r\n";
+                }
+
+                // Instancias el renderizador de las direcciones y vincularlo al mapa
+                var renderizadorDirecciones = new google.maps.DirectionsRenderer({
+                    map: googleMap,
+                    preserveViewport : true,
+                    suppressMarkers: true
+                });
+                renderizadorDirecciones.setDirections(response);
+                rutasActuales.push(renderizadorDirecciones);
+            } else {
+                mensajesError += 'Solicitud de direcciones falló: ' + status + "\r\n";
             }
-        ]
-    };
-
-    
-
-    // Get the HTML DOM element that will contain your map 
-    // We are using a div with id="map" seen below in the <body>
-    var mapElement = document.getElementById('map');
-
-    // Create the Google Map using out element and options defined above
-    var map = new google.maps.Map(mapElement, mapOptions);
-    
-    var addresses = ['New York'];
-
-    for (var x = 0; x < addresses.length; x++) {
-        $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
-            var p = data.results[0].geometry.location
-            var latlng = new google.maps.LatLng(p.lat, p.lng);
-            new google.maps.Marker({
-                position: latlng,
-                map: map,
-                icon: 'images/loc.png'
-            });
-
         });
     }
-    
+
+    // Dibujar un marcador en la ruta actual...
+    var marker1 = new google.maps.Marker({
+        position: { lat: posicionInicial.lat, lng: posicionInicial.lng },
+        map: googleMap,
+        title: "MySweetMoon"
+    });
+    marcadoresActuales.push(marker1);
+
+    if(pos!== null){
+        var marker2 = new google.maps.Marker({
+            position: { lat: pos.lat, lng: pos.lng },
+            map: googleMap,
+            title: "Tu ubicación"
+        });
+        marcadoresActuales.push(marker2);
+    }
 }
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+                          'Error: The Geolocation service failed.' :
+                          'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(googleMap);
+  }
+
+function leerMensajesError() {
+    return mensajesError;
+}
+
+function leerMensajesAdvertencia() {
+    return mensajesAdvertencia;
+}
+
 google.maps.event.addDomListener(window, 'load', init);
