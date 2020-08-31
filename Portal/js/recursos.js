aa02047a196da9266c5853680da0b2707a589bed
BryanManzano2016 <bryanmanzano2@gmail.com>
2020-08-30 21:37:52 -0500

    Recursos modificacion

--- Portal/js/recursos.js
+++ Portal/js/recursos.js
@@ -7,48 +7,64 @@ var recursosSeccion = new Vue({
   },
   methods: {
     cargarInformacionContacto: function () {
-      this.tipo = "contactos";
+      this.tipo = "administrarContactos"
       cargarContactosFetch().then((data) => {
-        this.resultados = JSON.parse(data);
-      });
+        this.resultados = JSON.parse(data)
+      })
     },
     cargarInformacionProductos: function () {
-      this.tipo = "productos";
+      this.tipo = "administrarProductos"
       cargarProductosFetch().then((data) => {
-        this.resultados = JSON.parse(data);
-      });
+        this.resultados = JSON.parse(data)
+      })
     },
-  },
-});
+    cargarInformacionNoticias: function () {
+      this.tipo = "administrarNoticias"
+      cargarNoticiasFetch().then((data) => {
+        this.resultados = JSON.parse(data)
+        console.log(this.resultados);
+      })
+    }
+  }
+})
 
-// funcion asincrona que conecta con servidor express
 async function cargarContactosFetch() {
   let response = await fetch("http://localhost:3000/contactos", {
     method: "GET",
-  });
-  var data = await response.text();
-  return data;
+  })
+  var data = await response.text()
+  return data
 }
 
-// funcion asincrona que conecta con servidor express
 async function cargarProductosFetch() {
   let response = await fetch("http://localhost:3000/productos", {
     method: "GET",
-  });
-  var data = await response.text();
-  return data;
+  })
+  var data = await response.text()
+  return data
+}
+
+async function cargarNoticiasFetch() {
+  let response = await fetch("http://localhost:3000/new/todos", {
+    method: "GET",
+  })
+  var data = await response.text()
+  return data
 }
 
 function tipoConsulta(valor) {
-  document.getElementById("recursosResultado").classList.remove('d-none');
+  document.getElementById("recursosResultado").classList.remove('d-none')
   switch (valor) {
-    case "productos":
-        recursosSeccion.cargarInformacionProductos();
-      break;
-    case "contactos":
-        recursosSeccion.cargarInformacionContacto();
-      break;
+    case "administrarProductos":
+      recursosSeccion.cargarInformacionProductos()
+      break
+    case "administrarContactos":
+      recursosSeccion.cargarInformacionContacto()
+      break
+    case "administrarNoticias":
+      recursosSeccion.cargarInformacionNoticias()
+      break
     default:
-      break;
+      break
   }
 }
